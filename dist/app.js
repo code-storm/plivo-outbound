cdc67ff223467d297e90eadd7bce24e5d3b7a7ba
Mudit Khandelwal <mudit-k>
2021-05-10 23:32:42 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,44 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const port = 3000;
+var plivo = require("plivo");
+var PhloClient = plivo.PhloClient;
+app.use(express_1.default.urlencoded({ extended: true }));
+var authId = "MAMWU3ZWM5YMZJNMY4ZM";
+var authToken = "NjcwYzIzYzQ2MDc0MWQ3NGQyYTMzMGQ1MmI0YzA1";
+var phloId = "9034bb69-2e8d-4311-875f-aa6230c33de4";
+var payload = {
+    from: "+18663820815",
+    to: null,
+};
+app.get("/", (req, res) => {
+    res.send("GET request is working");
+});
+app.post("/connect", (req, res) => {
+    if (!req.body.to) {
+        return res.status(500).json({
+            message: '"to" parameter not present.',
+        });
+    }
+    payload.to = req.body.to;
+    const phloClient = new PhloClient(authId, authToken);
+    phloClient
+        .phlo(phloId)
+        .run(payload)
+        .then(function (result) {
+        console.log("Phlo run result", result);
+    })
+        .catch(function (err) {
+        console.error("Phlo run failed", err);
+    });
+    return res.json({ success: true });
+});
+app.listen(port, () => {
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+const app = express();
+const port = 3000;
+var plivo = require("plivo");
+var PhloClient = plivo.PhloClient;
+
+app.use(express.urlencoded({ extended: true}));
+
+var authId = "MAMWU3ZWM5YMZJNMY4ZM";
+var authToken = "NjcwYzIzYzQ2MDc0MWQ3NGQyYTMzMGQ1MmI0YzA1";
+var phloId = "9034bb69-2e8d-4311-875f-aa6230c33de4";
+var payload = {
+  from: "+18663820815",
+  to: null,
+};
+
+app.get("/", (req, res) => {
+  res.send("GET request is working");
+});
+
+app.post("/connect", (req, res) => {
+  if (!req.body.to) {
+    return res.status(500).json({
+      message: '"to" parameter not present.',
+    });
+  }
+  payload.to = req.body.to;
+  const phloClient = new PhloClient(authId, authToken);
+  phloClient
+    .phlo(phloId)
+    .run(payload)
+    .then(function (result) {
+      console.log("Phlo run result", result);
+    })
+    .catch(function (err) {
+      console.error("Phlo run failed", err);
+    });
+  return res.json({ success: true });
+});
+
+app.listen(port, () => {
+  return console.log(`server is listening on ${port}`);
+});
