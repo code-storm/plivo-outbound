b133e0dbc998e8497fe1a6a0bd0c75ad56c4f887
Mudit Khandelwal <mudit-k>
2021-05-11 23:02:06 +0530

    added plivo package

--- dist/app.js
+++ dist/app.js
@@ -64,7 +64,34 @@ var authId = config_1.config.PLIVO_AUTH_ID;
 var authToken = config_1.config.PLIVO_AUTH_TOKEN;
 // var plivoClient = new plivo.Client(authId,authToken);
 app.get("/", cors_1.default(), (req, res) => {
-    res.send("GET request is working");
+    const method = "POST";
+    const url = "https://twin-node-server.herokuapp.com/event-hook";
+    const [nonce, auth_token, signature, params] = ["00112514496856822964", "NjcwYzIzYzQ2MDc0MWQ3NGQyYTMzMGQ1MmI0YzA1", "TLN7rOiDF+ZPjMP+FlDp/QMl3cwEWDI7vBYGjxkQpD0=", {
+            ALegRequestUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+            ALegUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+            AnswerTime: '2021-05-11 22:50:31',
+            BillDuration: '60',
+            BillRate: '0.032',
+            CallStatus: 'completed',
+            CallUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+            Direction: 'outbound',
+            Duration: '5',
+            EndTime: '2021-05-11 22:50:36',
+            Event: 'Hangup',
+            From: '918826211131',
+            HangupCause: 'ALLOTTED_TIMEOUT',
+            HangupCauseCode: '6000',
+            HangupCauseName: 'Scheduled Hangup',
+            HangupSource: 'Plivo',
+            ParentAuthID: 'MAMWU3ZWM5YMZJNMY4ZM',
+            RequestUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+            SessionStart: '2021-05-11 17:20:25.994819',
+            StartTime: '2021-05-11 22:50:22',
+            To: '918377803318',
+            TotalCost: '0.03200'
+        }];
+    var validate = plivo.validateV3Signature(method, url, nonce, auth_token, signature, params);
+    res.send("GET request is working" + validate);
 });
 app.post("/connect", cors_1.default(corsOptionsDelegate), (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
@@ -77,20 +104,26 @@ app.post("/connect", cors_1.default(corsOptionsDelegate), (req, res) => __awaite
     }
 }));
 app.post("/event-hook", (req, res) => {
-    console.log(">>>", req.headers['origin']);
     let headers = req.headers;
     console.log(headers);
-    let signature = headers["x-plivo-signature-v2"];
-    let nonce = headers["x-plivo-signature-v2-nonce"];
+    let signature = headers["x-plivo-signature-v3"];
+    let nonce = headers["x-plivo-signature-v3-nonce"];
+    console.log("before,", signature, nonce);
     if (!signature) {
         signature = "signature";
     }
     if (!nonce) {
         nonce = "12345";
     }
+    let url = req.url;
+    let auth_token = authToken;
+    console.log(signature, nonce);
+    let method = req.method;
+    let validate;
     let params = req.body;
-    const validate = plivo.validateSignature(req.url, nonce, signature, authToken);
-    console.log(validate);
+    console.log(method, url, nonce, auth_token, signature, params);
+    validate = plivo.validateV3Signature(method, url, nonce, auth_token, signature, params);
+    console.log(">>", validate);
     const plivoData = req.body;
     const outBoundCall = {
         callStartTime: plivoData.AnswerTime,
@@ -104,7 +137,6 @@ app.post("/event-hook", (req, res) => {
     };
     model_1.db.OutboundCalls.create(outBoundCall)
         .then((data) => {
-        console.log(data);
         res.json({ success: true });
     })
         .catch((err) => {
--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,33 @@ var authToken = config.PLIVO_AUTH_TOKEN;
 // var plivoClient = new plivo.Client(authId,authToken);
 
 app.get("/", cors(), (req, res) => {
-  res.send("GET request is working");
+  const method = "POST";
+  const url = "https://twin-node-server.herokuapp.com/event-hook";
+  const [nonce, auth_token, signature, params] = ["00112514496856822964", "NjcwYzIzYzQ2MDc0MWQ3NGQyYTMzMGQ1MmI0YzA1", "TLN7rOiDF+ZPjMP+FlDp/QMl3cwEWDI7vBYGjxkQpD0=", {
+    ALegRequestUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+    ALegUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+    AnswerTime: '2021-05-11 22:50:31',
+    BillDuration: '60',
+    BillRate: '0.032',
+    CallStatus: 'completed',
+    CallUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+    Direction: 'outbound',
+    Duration: '5',
+    EndTime: '2021-05-11 22:50:36',
+    Event: 'Hangup',
+    From: '918826211131',
+    HangupCause: 'ALLOTTED_TIMEOUT',
+    HangupCauseCode: '6000',
+    HangupCauseName: 'Scheduled Hangup',
+    HangupSource: 'Plivo',
+    ParentAuthID: 'MAMWU3ZWM5YMZJNMY4ZM',
+    RequestUUID: '4e7a1500-4240-4f75-aa5b-b995f0409299',
+    SessionStart: '2021-05-11 17:20:25.994819',
+    StartTime: '2021-05-11 22:50:22',
+    To: '918377803318',
+    TotalCost: '0.03200' }];
+    var validate = plivo.validateV3Signature(method, url, nonce, auth_token, signature, params);
+  res.send("GET request is working" + validate);
 });
 
 app.post("/connect", cors(corsOptionsDelegate), async (req, res) => {
@@ -51,14 +77,14 @@ app.post("/event-hook", (req, res) => {
     console.log(headers);
     let signature = headers["x-plivo-signature-v3"];
     let nonce = headers["x-plivo-signature-v3-nonce"];
-    console.log("before,", signature, nonce);
+    // console.log("before,", signature, nonce);
     if (!signature) {
         signature = "signature";
     }
     if (!nonce) {
         nonce = "12345";
     }
-    let url = req.url;
+    let url = req.protocol + "://" + req.get("host") + req.url;
     let auth_token = authToken;
     console.log(signature, nonce);
     let method = req.method;
